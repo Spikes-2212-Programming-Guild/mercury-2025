14b0bcbe408f9e5a1affa7fb9fc8e03148819d2c
itay <itay2318@gmail.com>
2025-10-03 20:46:44 +0300

    cleaned up the css

--- docs/config/constants.js
+++ docs/config/constants.js
@@ -1,26 +1,6 @@
-const COLORS = {
-    VALID: 'green',
-    INVALID: 'red',
-    NEUTRAL: 'white',
-    WARNING: 'yellow',
-    ACTIVE: 'white',
-    INACTIVE: 'gray',
-};
-
 export const SCOREBOX_MIN_VALUE = 0;
 export const SCOREBOX_MAX_VALUE = 100;
 export const SCOREBOX_DEFAULT_VALUE = 0;
 
-const RETRY_SENDING_SUBMISSIONS_INTERVAL_MS = 1000 * 60 * 4; // 4 minutes
-
 export const APP_ID = "qp"
-
-const TELEOP_START_TIME_MS = 1000 * 15; // 15 seconds
-
 const API_URL = "https://mercury-2025-server.onrender.com/submit";
-
-const RESET_TYPES = {
-    CLEAR: 'clear',
-    KEEP: 'keep',
-    INCREASE: 'increase',
-};
