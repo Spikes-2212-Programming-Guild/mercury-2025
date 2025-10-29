4e8d3f6fb99a8fe16b70e8419859b5a95bc9cdba
itay <itay2318@gmail.com>
2025-10-29 10:37:46 +0200

    small fixes

--- newServer.js
+++ newServer.js
@@ -6,10 +6,6 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "docs")));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "docs", "index.html"));
-});
-
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
--- oldServer.js
+++ oldServer.js
@@ -4,13 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Serve everything in "old" as static files
 app.use(express.static(path.join(__dirname, "old")));
-
-// Default route → serve index.html
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "old", "index.html"));
-});
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
